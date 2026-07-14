2c9f46ba97e3bbf2e09a1dd35dc639ed4e20fc43
華帆 <mkaho0919kaho.wk@gmail.com>
2026-07-14 20:09:45 +0900

    お知らせ追加（未来サポーター育成募集）

---
+++ content/news/2026-07-14-mirai-supporter-recruitment.md
@@ -0,0 +1,7 @@
+---
+title: "ミライサポーター育成プロジェクト参加者募集開始！"
+date: "2026-07-14"
+category: "recruitment"
+published: true
+externalUrl: "https://www.city.sabae.fukui.jp/event/sonota/SogoSeisakumirasapo.html"
+---
--- src/lib/generated/news.ts
+++ src/lib/generated/news.ts
@@ -21,4 +21,12 @@ published: true
 
 今後ともよろしくお願いいたします。
 `,
+  "2026-07-14-mirai-supporter-recruitment": `---
+title: "ミライサポーター育成プロジェクト参加者募集開始！"
+date: "2026-07-14"
+category: "recruitment"
+published: true
+externalUrl: "https://www.city.sabae.fukui.jp/event/sonota/SogoSeisakumirasapo.html"
+---
+`,
 };
