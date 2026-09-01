7c3ea3925d7a50077f4e5eb194e649cf5c90339e
華帆 <mkaho0919kaho.wk@gmail.com>
2026-09-01 22:36:01 +0900

    お知らせ追加

--- src/lib/generated/news.ts
+++ src/lib/generated/news.ts
@@ -61,4 +61,16 @@ href: "/ticket"
 
 [入場券申込概要はこちら](/ticket)
 `,
+  "2026-09-04-booth-list": `---
+title: "出展ブース一覧を公開しました"
+date: "2026-09-04"
+category: "general"
+published: true
+href: "/booths"
+---
+
+出展ブース一覧を公開しました。
+
+[ブース一覧はこちら](/booths)
+`,
 };
